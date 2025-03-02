a0cd0542f6445d1b5590a5cdbd7b0a8d06123b4f
kawsar hussain <akibhssain22@gmail.com>
2025-03-02 21:12:07 +0600

    outher file add

---
+++ index1.js
@@ -0,0 +1,21 @@
+// fix mobile
+const cardbtn = document.getElementById("fix-Mobile");
+cardbtn.addEventListener("click", function () {
+  allwork("fix-Mobile");
+})
+
+//Add Dark
+const addDark = document.getElementById("addDark");
+addDark.addEventListener("click", function () {
+  allwork("addDark");
+})
+// Optimize 
+const Optimize  = document.getElementById("Optimize ");
+Optimize .addEventListener("click", function () {
+  allwork("Optimize ");
+})
+// Addnew
+const Addnew  = document.getElementById("Addnew");
+Addnew .addEventListener("click", function () {
+  allwork("Addnew");
+})
