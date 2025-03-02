1d3bbe32ca719f7e17c41fce8ae8339463d5c4cc
kawsar hussain <akibhssain22@gmail.com>
2025-03-02 23:59:47 +0600

    add

--- index.js
+++ index.js
@@ -1,70 +1,70 @@
-// string converter
-function stringconverter(strintdata) {
+// fix mobile
+const cardbtn = document.getElementById("fix-Mobile");
+cardbtn.addEventListener("click", function () {
+  allwork("fix-Mobile");
+})
 
-    const nav_data = document.getElementById(strintdata).innerHTML;
-   const change= parseInt(nav_data);
-    return change;
-  }
-  
-  
-  
-  // web element add
-  
-  function webNumberAdd(strintdata, value) {
-  
-    document.getElementById(strintdata).innerHTML = value;
-  
-  }
-  
-  // drive card's button to parent 
-  function cardeliment(btnId) {
-    const cardinfo = document.getElementById(btnId).parentElement.parentElement.children[1].innerHTML;
-    return cardinfo;
-    
-  }
-  
-  // history add
-  function addHistoryEliment(cardtilte) {
-    let carddata = cardeliment(cardtilte);  
-    const time = new Date().toLocaleTimeString();
-  
-    document.getElementById("history").innerHTML += 
-      `<div class="bg-[#F4F7FF] rounded-2xl p-5 m-3">You have completed the task ${carddata} at ${time}</div>`;
-  }
-  
-  
-  
-  
-  // main function
-  function allwork(cardid) {
-    let navInt = stringconverter("nav23");  
-    let taskInt = stringconverter("taskAssigned");
-  
-   
-  
-    navInt = Number(navInt);
-    taskInt = Number(taskInt);
-  
-  
-    
-  
-    alert("It's ok");
-  
-    if (taskInt === 1) {
-      alert("Congratulations");
-    }
-  
-    navInt += 1;  
-    taskInt -= 1;  
-  
-    console.log(typeof navInt, typeof taskInt);
-    console.log("Updated taskInt:", taskInt);
-  
-    webNumberAdd("nav23", navInt); 
-    webNumberAdd("taskAssigned", taskInt);
-    
-    addHistoryEliment(cardid);  
-    document.getElementById(cardid).disabled = true;
-    document.getElementById(cardid).style.backgroundColor ="#F4F7FF" ;
-  
-  }
+//Add Dark
+const addDark = document.getElementById("addDark");
+addDark.addEventListener("click", function () {
+  allwork("addDark");
+})
+// Optimize 
+const Optimize = document.getElementById("Optimize ");
+Optimize .addEventListener("click", function () {
+  allwork("Optimize ");
+})
+// Addnew
+const Addnew = document.getElementById("Addnew");
+Addnew .addEventListener("click", function () {
+  allwork("Addnew");
+})
+// Integrate
+const Integrate = document.getElementById("Integrate");
+Integrate .addEventListener("click", function () {
+  allwork("Integrate");
+})
+// Improve
+const Improve = document.getElementById("Improve");
+Improve .addEventListener("click", function () {
+  allwork("Improve");
+})
+
+// clear history 
+const clear = document.getElementById("clear");
+
+clear.addEventListener('click', function () {
+  const history = document.getElementById("history");
+
+  if (history) {
+    history.innerHTML = ""; // Clears all child elements inside history
+  } 
+});
+
+// date
+const tody = document.getElementById("date");
+const d = new Date();
+tody.innerHTML = d.toDateString();
+
+// discover
+// const question = document.getElementById("discover");
+// question.addEventListener('click', function () {
+// window.location.href= "./questionPage.html";
+// })
+
+
+// backToDesk
+// const back = document.getElementById("backToDesk");
+// back.addEventListener('click', function () {
+// window.location.href = "./index.html";
+// })
+
+
+
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+document.getElementById("thinm").addEventListener("click", function () {
+    document.body.style.backgroundColor = getRandomColor();
+});
--- index1.js
+++ index1.js
@@ -1,21 +1,70 @@
-// fix mobile
-const cardbtn = document.getElementById("fix-Mobile");
-cardbtn.addEventListener("click", function () {
-  allwork("fix-Mobile");
-})
+// string converter
+function stringconverter(strintdata) {
 
-//Add Dark
-const addDark = document.getElementById("addDark");
-addDark.addEventListener("click", function () {
-  allwork("addDark");
-})
-// Optimize 
-const Optimize  = document.getElementById("Optimize ");
-Optimize .addEventListener("click", function () {
-  allwork("Optimize ");
-})
-// Addnew
-const Addnew  = document.getElementById("Addnew");
-Addnew .addEventListener("click", function () {
-  allwork("Addnew");
-})
+  const nav_data = document.getElementById(strintdata).innerHTML;
+ const change= parseInt(nav_data);
+  return change;
+}
+
+
+
+// web element add
+
+function webNumberAdd(strintdata, value) {
+
+  document.getElementById(strintdata).innerHTML = value;
+
+}
+
+// drive card's button to parent 
+function cardeliment(btnId) {
+  const cardinfo = document.getElementById(btnId).parentElement.parentElement.children[1].innerHTML;
+  return cardinfo;
+  
+}
+
+// history add
+function addHistoryEliment(cardtilte) {
+  let carddata = cardeliment(cardtilte);  
+  const time = new Date().toLocaleTimeString();
+
+  document.getElementById("history").innerHTML += 
+    `<div class="bg-[#F4F7FF] rounded-2xl p-5 m-3">You have completed the task ${carddata} at ${time}</div>`;
+}
+
+
+
+
+// main function
+function allwork(cardid) {
+  let navInt = stringconverter("nav23");  
+  let taskInt = stringconverter("taskAssigned");
+
+ 
+
+  navInt = Number(navInt);
+  taskInt = Number(taskInt);
+
+
+  
+
+  alert("It's ok");
+
+  if (taskInt === 1) {
+    alert("Congratulations");
+  }
+
+  navInt += 1;  
+  taskInt -= 1;  
+
+  console.log(typeof navInt, typeof taskInt);
+  console.log("Updated taskInt:", taskInt);
+
+  webNumberAdd("nav23", navInt); 
+  webNumberAdd("taskAssigned", taskInt);
+  
+  addHistoryEliment(cardid);  
+  document.getElementById(cardid).disabled = true;
+  document.getElementById(cardid).style.backgroundColor ="#F4F7FF" ;
+
+}
